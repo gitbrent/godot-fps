3598b3bf34bef4696e1b231a1c2fda4e9b04a448
Brent Ely <gitbrent@users.noreply.github.com>
2025-05-15 21:22:19 -0500

    WIP

--- enemy_state/state_dead.gd
+++ enemy_state/state_dead.gd
@@ -2,10 +2,18 @@ extends EnemyState
 class_name EnemyDead
 
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
+@onready var ow_my_balls: AudioStreamPlayer = $"../../Audio/ow-my-balls"
 
 func enter() -> void:
 	print("DEAD!")
-	animation_player.play("DYING")
+	# 1: normal
+	#animation_player.play("DYING")
+	# --OR--
+	# 2: burtal way to go
+	ow_my_balls.play()
+	animation_player.play("animations-test05/BRUTAL_DEATH")
+
+	# 3:
 	await animation_player.animation_finished
 	# FIXME: queue_free()
 	# send signal to parent to fade away and queu free??
