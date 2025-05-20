7f35f8de024efeea7eeac4ff75ea087a8d14a844
Brent Ely <gitbrent@users.noreply.github.com>
2025-05-20 17:42:35 -0500

    updated direction & look

--- enemy_controller/state_idle.gd
+++ enemy_controller/state_idle.gd
@@ -13,6 +13,8 @@ func physics_update(delta: float) -> Vector3:
 	var players = get_tree().get_nodes_in_group("player")
 	var nearest_player_distance = INF
 	var nearest_player: CharacterBody3D = null
+	var desired_horizontal_velocity = Vector3.ZERO
+	
 	# 1:
 	for player in players:
 		if player is CharacterBody3D:
@@ -20,11 +22,19 @@ func physics_update(delta: float) -> Vector3:
 			if distance < nearest_player_distance:
 				nearest_player_distance = distance
 				nearest_player = player
-
+	
 	# 2: If a player is found within the detection range, transition to follow
 	if nearest_player and nearest_player_distance <= detection_range:
 		if enemy_controller.can_see(nearest_player):
 			transitioned.emit(self, "attack")
 	
+	# If a threat direction has been set (e.g., after taking damage)
+	if enemy_controller.last_known_threat_direction != Vector3(0,0,1): # Check against default
+		# Face the direction of threat
+		desired_rotation_direction = enemy_controller.last_known_threat_direction
+		# ^^^ WORKS great
+		#desired_horizontal_velocity = enemy_controller.last_known_threat_direction * 4
+		# FIXME: Above causes 1000's of errors
+	
 	# 3:
-	return Vector3.ZERO
+	return desired_horizontal_velocity
