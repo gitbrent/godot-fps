2a70d93a2ae513b2129c04096f3bb03f21d598a2
Brent Ely <gitbrent@users.noreply.github.com>
2025-05-25 14:19:06 -0500

    updated

--- ui/hud_game_menu.gd
+++ ui/hud_game_menu.gd
@@ -1,21 +1,32 @@
 extends Control
 
 signal btn_clicked_start_game
+signal btn_clicked_show_gallery
 
+#region vars
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
+#endregion
 
 func _ready() -> void:
 	canvas_layer.visible = false
 
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		_on_button_start_pressed()
+	elif Input.is_action_just_pressed("ui_select"):
+		_on_button_gallery_pressed()
+
+# HANDLERS ================================================
+
 func _on_button_start_pressed() -> void:
 	emit_signal("btn_clicked_start_game")
 
-# PUBLIC ===============================
+func _on_button_gallery_pressed() -> void:
+	emit_signal("btn_clicked_show_gallery")
+	#get_parent()._on_examine_weapon_button_pressed()
+	#pass # Replace with function body.
+
+# PUBLIC ==================================================
 
 func show_game_menu(show:bool) -> void:
 	canvas_layer.visible = show
-
-
-func _on_button_gallery_pressed() -> void:
-	get_parent()._on_examine_weapon_button_pressed()
-	pass # Replace with function body.
