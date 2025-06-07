926b0d64c0f04ef457e93be8c4b1f39ec21089ff
Brent Ely <gitbrent@users.noreply.github.com>
2025-06-07 14:01:17 -0500

    added new enemy gallery button and new button assets

--- ui/hud_game_menu.gd
+++ ui/hud_game_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 signal btn_clicked_start_game
 signal btn_clicked_show_gallery
+signal btn_clicked_show_enemy
 
 #region vars
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
@@ -15,6 +16,8 @@ func _unhandled_input(event):
 		_on_button_start_pressed()
 	elif Input.is_action_just_pressed("ui_select"):
 		_on_button_gallery_pressed()
+	elif Input.is_action_just_pressed("shooter_reload"):
+		_on_button_enemy_pressed()
 
 # HANDLERS ================================================
 
@@ -23,6 +26,9 @@ func _on_button_start_pressed() -> void:
 
 func _on_button_gallery_pressed() -> void:
 	emit_signal("btn_clicked_show_gallery")
+
+func _on_button_enemy_pressed() -> void:
+	emit_signal("btn_clicked_show_enemy")
 
 # PUBLIC ==================================================
 
